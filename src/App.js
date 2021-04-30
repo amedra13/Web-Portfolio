7cd5e113ee102fa53590c02a8ac6e85f6a625110
Andres Medrano <amedra13@gmail.com>
2021-04-30 00:18:35 -0700

    styling animations with reat router/ transition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './styles/App.scss';
+import Sidebar from './components/Sidebar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Skills from './pages/Skills';
+import Work from './pages/Work';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 
 function App() {
+	let location = useLocation();
 	return (
 		<div className="app">
-			<h1>App Component</h1>
+			<Sidebar />
+			<TransitionGroup component={null}>
+				<CSSTransition
+					key={location.key}
+					unmountOnExit
+					classNames="fade"
+					timeout={1000}
+				>
+					<Switch location={location}>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/contact" component={Contact} />
+						<Route exact path="/skills" component={Skills} />
+						<Route exact path="/work" component={Work} />
+						<Redirect to="/" />
+					</Switch>
+				</CSSTransition>
+			</TransitionGroup>
 		</div>
 	);
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
 	return (
 		<div className="sidebar">
 			<h1>Sidebar Component</h1>
+			<div className="sidebar__links">
+				<NavLink to="/home" activeClassName="selected">
+					Home
+				</NavLink>
+				<NavLink to="/about" activeClassName="selected">
+					About
+				</NavLink>
+				<NavLink to="/skills" activeClassName="selected">
+					Skills
+				</NavLink>
+				<NavLink to="/work" activeClassName="selected">
+					Work
+				</NavLink>
+				<NavLink to="/contact" activeClassName="selected">
+					Contact
+				</NavLink>
+			</div>
 		</div>
 	);
 };
