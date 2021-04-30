21318b5160174cecde3d6c47781cda5950bed3ff
Andres Medrano <amedra13@gmail.com>
2021-04-30 14:16:30 -0700

    begin work on styling sidebar

--- src/App.js
+++ src/App.js
@@ -15,25 +15,23 @@ function App() {
 		<div className="app">
 			<Sidebar />
 			<div className="app__rightSide">
-				<div className="app__rightContainer">
-					<TransitionGroup component={null}>
-						<CSSTransition
-							key={location.key}
-							unmountOnExit
-							classNames="fade"
-							timeout={2000}
-						>
-							<Switch location={location}>
-								<Route exact path="/" component={Home} />
-								<Route exact path="/about" component={About} />
-								<Route exact path="/contact" component={Contact} />
-								<Route exact path="/skills" component={Skills} />
-								<Route exact path="/work" component={Work} />
-								<Redirect to="/" />
-							</Switch>
-						</CSSTransition>
-					</TransitionGroup>
-				</div>
+				<TransitionGroup component={null}>
+					<CSSTransition
+						key={location.key}
+						unmountOnExit
+						classNames="fade"
+						timeout={2000}
+					>
+						<Switch location={location}>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/about" component={About} />
+							<Route exact path="/contact" component={Contact} />
+							<Route exact path="/skills" component={Skills} />
+							<Route exact path="/work" component={Work} />
+							<Redirect to="/" />
+						</Switch>
+					</CSSTransition>
+				</TransitionGroup>
 			</div>
 		</div>
 	);
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import Logo from '../images/logo.png';
 import { NavLink } from 'react-router-dom';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Sidebar = () => {
 	return (
 		<div className="sidebar">
-			<h1>Sidebar Component</h1>
-			<div className="sidebar__links">
-				<NavLink to="/home" activeClassName="selected">
-					Home
-				</NavLink>
-				<NavLink to="/about" activeClassName="selected">
-					About
-				</NavLink>
-				<NavLink to="/skills" activeClassName="selected">
-					Skills
-				</NavLink>
-				<NavLink to="/work" activeClassName="selected">
-					Work
-				</NavLink>
-				<NavLink to="/contact" activeClassName="selected">
-					Contact
-				</NavLink>
+			<div className="sidebar__container">
+				<img src={Logo} alt="amsportscards" />
+				<div className="sidebar__links">
+					<NavLink to="/home" activeClassName="selected">
+						Home
+					</NavLink>
+					<NavLink to="/about" activeClassName="selected">
+						About
+					</NavLink>
+					<NavLink to="/skills" activeClassName="selected">
+						Skills
+					</NavLink>
+					<NavLink to="/work" activeClassName="selected">
+						Work
+					</NavLink>
+					<NavLink to="/contact" activeClassName="selected">
+						Contact
+					</NavLink>
+				</div>
+				<div className="sidebar__iconLinks">
+					<LinkedInIcon />
+					<InstagramIcon />
+					<TwitterIcon />
+					<GitHubIcon />
+				</div>
 			</div>
 		</div>
 	);
