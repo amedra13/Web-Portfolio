8a504ccd03d32142175ff95cf30ca0deaf6f3d62
Andres Medrano <amedra13@gmail.com>
2021-04-29 23:07:24 -0700

    cra clean up and install packages

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			<h1>Sidebar Component</h1>
+		</div>
+	);
+};
+
+export default Sidebar;
---
+++ src/pages/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+	return (
+		<div className="skills">
+			<h1>Skills Component</h1>
+		</div>
+	);
+};
+
+export default Skills;
