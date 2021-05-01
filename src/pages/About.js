df240f58ca505d55a636d003e8a585c4a3c548b3
Andres Medrano <amedra13@gmail.com>
2021-04-30 17:22:05 -0700

    begin styling about component

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,23 @@ import React from 'react';
 const About = () => {
 	return (
 		<div className="about">
-			<h1>About Component</h1>
+			<div className="about__container">
+				<h1>Who am I ?</h1>
+				<p>
+					I am a front-end developer passionate with React.js! I am a problem
+					solver, and I love being able to create responsive effects.
+				</p>
+				<p>
+					I am a proud hispanic currently residing in the Bay Area. Aside from
+					my days in coding, I also love sports, travelling, outdoor activities,
+					and my family.
+				</p>
+				<p>
+					Ever since I’ve entered the world of coding, I’ve been sucked in
+					since, and I hope to find the opportunity to grow and learn more as a
+					developer.
+				</p>
+			</div>
 		</div>
 	);
 };
