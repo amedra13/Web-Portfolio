f909f8120327385c6006f01b278e692249ec1bb5
Andres Medrano <amedra13@gmail.com>
2021-05-03 00:55:15 -0700

    Use material grid and skills component responsive

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../data/projectsData.json';
+import Grid from '@material-ui/core/Grid';
 
 const Project = () => {
 	let { project } = useParams();
@@ -10,38 +11,46 @@ const Project = () => {
 			<div className="project__header">
 				<h1>{data[project].name}</h1>
 			</div>
-			<div className="project__highlights">
-				<div className="highlights__description">
-					<h2>{data[project].highlightsIntro}</h2>
-					<p style={{ textDecoration: 'underline' }}>
-						Highlights and goals achieved:
-					</p>
-					<ul>
-						{data[project].highlightsList.map((item) => (
-							<li>{item}</li>
-						))}
-					</ul>
-				</div>
-				<div className="highlights__imgContainer">
-					<img src={data[project].image} alt="" />
-				</div>
-			</div>
-			<div className="project__improvements">
-				<div className="highlights__imgContainer">
-					<p>Image placed here</p>
-				</div>
-				<div className="highlights__description">
-					<h2>{data[project].improvementsIntro}</h2>
-					<p style={{ textDecoration: 'underline' }}>
-						Notes for Improvements:{' '}
-					</p>
-					<ul>
-						{data[project].improvementsList.map((item) => (
-							<li>{item}</li>
-						))}
-					</ul>
-				</div>
-			</div>
+			<Grid container className="project__highlights">
+				<Grid item xs={12} md={8}>
+					<div className="highlights__description">
+						<h2>{data[project].highlightsIntro}</h2>
+						<p style={{ textDecoration: 'underline' }}>
+							Highlights and goals achieved:
+						</p>
+						<ul>
+							{data[project].highlightsList.map((item, i) => (
+								<li key={i}>{item}</li>
+							))}
+						</ul>
+					</div>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<div className="highlights__imgContainer">
+						<img src={data[project].image} alt="" />
+					</div>
+				</Grid>
+			</Grid>
+			<Grid container className="project__improvements">
+				<Grid item xs={12} md={4}>
+					<div className="improvements__imgContainer">
+						<p>Image placed here</p>
+					</div>
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<div className="improvements__description">
+						<h2>{data[project].improvementsIntro}</h2>
+						<p style={{ textDecoration: 'underline' }}>
+							Notes for Improvements:{' '}
+						</p>
+						<ul>
+							{data[project].improvementsList.map((item, i) => (
+								<li key={i}>{item}</li>
+							))}
+						</ul>
+					</div>
+				</Grid>
+			</Grid>
 			<div className="project__link">
 				<a href={data[project].link} target="_blank" rel="noreferrer">
 					Link to Project{' '}
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -10,74 +10,84 @@ import NodeLogo from '../images/nodeLogo.png';
 import SassLogo from '../images/sassLogo.png';
 import ReactRouterLogo from '../images/reactRouterLogo.png';
 import NpmLogo from '../images/npmLogo.png';
+import Grid from '@material-ui/core/Grid';
 
 const Skills = () => {
 	return (
 		<div className="skills">
-			<div className="skills__header">
-				<h1>Wondering what tools I have used?</h1>
-			</div>
-			<div className="skills__description">
-				<h1>Start of Coding Journey</h1>
-				<div className="description__body">
-					<p>
-						Since beginning my journey learing to code nearly 3 years ago, I’ve
-						taken traditional classes, online tutorials, and multiple Udemy
-						courses, to teach myself how to code and create great looking
-						applications
-					</p>
-					<p>
-						I love to learn new technologies and apply them to new projects and
-						see it all come together to fruition. One of the greatest
-						satisfations is the moment a new websites and applications that are
-						fast and easy to use, go live. The main area of my expertise is
-						front-end development, HTML, CSS, JS, React, and building new
-						features and animations
-					</p>
+			<Grid container spacing={2} className="skills__grid">
+				<Grid item xs={12}>
+					<div className="skills__header">
+						<h1>Wondering what tools I have used?</h1>
+					</div>
+				</Grid>
 
-					<p>
-						I also have experience working with the backend such as Node and
-						Express, and connecting my applications to a database such as
-						MongoDB and Firebase.
-					</p>
-					<p>Feel free to contact me, if you would like to know more.</p>
-				</div>
-			</div>
-			<div className="skills__icons">
-				<div className="icon__container" style={{ animationDelay: '2.2s' }}>
-					<img src={JavascriptLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '2.4s' }}>
-					<img src={CssLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '2.6s' }}>
-					<img src={HtmlLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '2.8s' }}>
-					<img src={SassLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '3s' }}>
-					<img src={ReactLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '3.2s' }}>
-					<img src={ReduxLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '3.4s' }}>
-					<img src={ReactRouterLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '3.6s' }}>
-					<img src={MongoDbLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '3.8s' }}>
-					<img src={NodeLogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '4s' }}>
-					<img src={MaterialUILogo} alt="" />
-				</div>
-				<div className="icon__container" style={{ animationDelay: '4.2s' }}>
-					<img src={NpmLogo} alt="" />
-				</div>
-			</div>
+				<Grid item md={12} lg={6} className="skills__item">
+					<div className="skills__description">
+						<h1>Start of Coding Journey</h1>
+						<div className="description__body">
+							<p>
+								Since beginning my journey learing to code nearly 3 years ago,
+								I’ve taken traditional classes, online tutorials, and multiple
+								Udemy courses, to teach myself how to code and create great
+								looking applications
+							</p>
+							<p>
+								I love to learn new technologies and apply them to new projects
+								and see it all come together to fruition. One of the greatest
+								satisfations is the moment a new websites and applications that
+								are fast and easy to use, go live. The main area of my expertise
+								is front-end development, HTML, CSS, JS, React, and building new
+								features and animations
+							</p>
+
+							<p>
+								I also have experience working with the backend such as Node and
+								Express, and connecting my applications to a database such as
+								MongoDB and Firebase.
+							</p>
+							<p>Feel free to contact me, if you would like to know more.</p>
+						</div>
+					</div>
+				</Grid>
+				<Grid item md={12} lg={6}>
+					<div className="skills__icons">
+						<div className="icon__container" style={{ animationDelay: '2.2s' }}>
+							<img src={JavascriptLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '2.4s' }}>
+							<img src={CssLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '2.6s' }}>
+							<img src={HtmlLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '2.8s' }}>
+							<img src={SassLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '3s' }}>
+							<img src={ReactLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '3.2s' }}>
+							<img src={ReduxLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '3.4s' }}>
+							<img src={ReactRouterLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '3.6s' }}>
+							<img src={MongoDbLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '3.8s' }}>
+							<img src={NodeLogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '4s' }}>
+							<img src={MaterialUILogo} alt="" />
+						</div>
+						<div className="icon__container" style={{ animationDelay: '4.2s' }}>
+							<img src={NpmLogo} alt="" />
+						</div>
+					</div>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
