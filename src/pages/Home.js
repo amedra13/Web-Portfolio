3ae73fe022c8350ea51cb24d155827ec76db9bdf
Andres Medrano <amedra13@gmail.com>
2021-05-19 17:18:35 -0700

    new fonts and color changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 			<div className="home__header">
 				<h2>Welcome to my portfolio!</h2>
 				<h2>My name is</h2>
-				<h1>Andres,</h1>
+				<h1>Andres</h1>
 				<p>a Frontend Developer</p>
 			</div>
 			<div className="home__contact">
